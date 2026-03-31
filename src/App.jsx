ec34e92e29aa6e646b70b094acbe711d7a1adf73
Somotosh <somotosh212@gmail.com>
2026-03-31 09:06:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {  } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-8xl'>hello</h1>
+    <p>hello</p>
+    <h1>hello</h1>
+    </>
+
+  )
+}
+
+export default App
